b31da98b834d39696f4f27d3e035690cc9e37eab
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-06-05 10:02:59 +0200

    page profile

--- src/Controller/User/UserPageController.php
+++ src/Controller/User/UserPageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\User;
 
+use Exception;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Intl\Countries; // permet de récuéprer les codes pays ISO et leurs noms
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UserPageController extends AbstractController {
 
@@ -26,6 +32,52 @@ class UserPageController extends AbstractController {
 
         return $this->render('user/dashboard.html.twig');
 	}
+
+	#[Route('/user/profile', name: 'user_profile')]
+	public function editProfile(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+	{
+		/** @var User $user */
+		$user = $this->getUser();
+
+		if (!$user) {
+			throw $this->createAccessDeniedException();
+		}
+
+		$countries = Countries::getNames('en');
+
+		if ($request->isMethod('POST')) {
+			$user->setName($request->request->get('name'));
+			$user->setSurname($request->request->get('surname'));
+			$user->setCountry($request->request->get('country'));
+			$user->setCity($request->request->get('city'));
+			$user->setPhoneNumber($request->request->get('phoneNumber'));
+			$user->setDescription($request->request->get('description'));
+
+			// Gestion upload photo
+			$photoFile = $request->files->get('picture-profile');
+			if ($photoFile) {
+				$filename = $slugger->slug(pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME));
+				$filename = $filename . '-' . uniqid() . '.' . $photoFile->guessExtension();
+				$photoFile->move($this->getParameter('user_pictures_directory'), $filename);
+
+				// Supprime l'ancienne si elle existe
+				if ($user->getPicturePath()) {
+					@unlink($this->getParameter('user_pictures_directory').'/'.$user->getPicturePath());
+				}
+				$user->setPicturePath($filename);
+			}
+
+			$em->flush();
+
+			$this->addFlash('success', 'Profile updated!');
+			return $this->redirectToRoute('user_profile');
+		}
+
+		return $this->render('user/profile.html.twig', [
+			'countries' => $countries,
+		]);
+	}
+
 
 }
 
